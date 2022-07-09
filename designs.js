bc2268f7d034b28cfad2f61b8ec36e3fa15fe688
Taalaibek Ashirov <taalaibek@gmail.com>
2022-07-09 00:34:12 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,49 @@
-// Select color input
-// Select size input
+const inputHeight = document.getElementById("inputHeight");
+const inputWidth = document.getElementById("inputWidth");
+const colorPicker = document.getElementById("colorPicker");
+const pixelCanvas = document.getElementById("pixelCanvas");
+const sizePicker = document.getElementById("sizePicker");
 
-// When size is submitted by the user, call makeGrid()
+// add submit event listener to the grid size picker form
+sizePicker.addEventListener("submit", function(e) {
+    e.preventDefault();
+    makeGrid(inputHeight.value, inputWidth.value);
+});
 
-function makeGrid() {
+function changeColor(e) {
+    e.target.style.backgroundColor = colorPicker.value;
+}
 
-// Your code goes here!
+/**
+* @description Creates grid
+* @param {number} gridHeight - Total number of rows
+* @param {number} gridWidth - Total number of columns
+*/
+function makeGrid(gridHeight, gridWidth) {
+    // reset the grid
+    while(pixelCanvas.firstChild) {
+        pixelCanvas.removeChild(pixelCanvas.firstChild);
+    }
 
+    // create the grid
+    for(let row = 1; row <= gridHeight; row++) {
+        // create row
+        const tr = document.createElement("tr");
+        
+        for(let column = 1; column <= gridWidth; column++) {
+            // create cell
+            const td = document.createElement("td");
+
+            // add click event listener to the cell
+            td.addEventListener("click", changeColor);
+            
+            // append cell to row
+            tr.appendChild(td);
+        }
+
+        // append row to grid
+        pixelCanvas.appendChild(tr);
+    }
+
+    
 }
